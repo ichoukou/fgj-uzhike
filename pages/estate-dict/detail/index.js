9a35e956caf57756eecae2fb1ed27793e52a6260
licong96 <licong19961204@.gmail.com>
2018-07-19 19:14:10 +0800

    start estate-detail

--- pages/estate-dict/detail/index.js
+++ pages/estate-dict/detail/index.js
@@ -5,28 +5,48 @@ Page({
    * 页面的初始数据
    */
   data: {
-  
+    src: 'http://app.vipfgj.com/upfile/20180423/8465CEA278D34DC298FA7B87C7D908A9.jpg',
+    swiper: {
+      imgUrls: [
+        'http://app.vipfgj.com/upfile/20180423/8465CEA278D34DC298FA7B87C7D908A9.jpg',
+        'http://img02.tooopen.com/images/20150928/tooopen_sy_143912755726.jpg',
+        'http://img06.tooopen.com/images/20160818/tooopen_sy_175866434296.jpg',
+      ],
+      indicatorDots: false,
+      autoplay: false,
+      interval: 5000,
+      duration: 500
+    },
+    map: {
+      controls: [{
+        id: 1,
+        // iconPath: '/resources/location.png',
+        position: {
+          left: 0,
+          top: 300 - 50,
+          width: 50,
+          height: 50
+        },
+        clickable: true
+      }],
+      markers: '',
+      polyline: ''
+    }
   },
-
-  /**
-   * 生命周期函数--监听页面加载
-   */
   onLoad: function (options) {
-  
+    
   },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
   onReady: function () {
   
   },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
   onShow: function () {
   
+  },
+  controltap(e) {
+
+  },
+  controltap(e) {
+
   },
 
   /**
